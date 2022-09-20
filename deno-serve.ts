3b41467d2c4757a1ac136e26dd0087651e073bda
dwn <dnielsen@biltmore.com>
2022-09-20 15:38:13 -0400

    update

--- deno-serve.ts
+++ deno-serve.ts
@@ -2,7 +2,21 @@ import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
 const app = new Application();
 const router = new Router();
 const domain = 'greenhouse.io';
-const sheetsURL = 'https://docs.google.com/spreadsheets/d/16mzGwtFLYBY2CDA3F8cSY6-iqQW8Ed4XeofeGVqqQAw/edit#gid=0'
+
+
+
+import xlsx from 'xlsx';
+import drive from 'drive-db';
+(async () => {
+  const data = await drive('16mzGwtFLYBY2CDA3F8cSY6-iqQW8Ed4XeofeGVqqQAw');
+  const workbook = xlsx.utils.book_new();
+  const worksheet = xlsx.utils.json_to_sheet(data);
+  xlsx.utils.book_append_sheet(workbook, worksheet);
+  xlsx.writeFile(workbook, 'test.xlsx');
+})();
+
+// const sheetsURL = 'https://docs.google.com/spreadsheets/d/16mzGwtFLYBY2CDA3F8cSY6-iqQW8Ed4XeofeGVqqQAw/edit#gid=0'
+
 
 
 router
