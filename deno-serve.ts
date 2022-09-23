695fe6f526287664f23a69ec6179164b4fe087ee
dwn <dnielsen@biltmore.com>
2022-09-23 14:44:31 -0400

    update

--- deno-serve.ts
+++ deno-serve.ts
@@ -9,15 +9,15 @@ console.log('STARTING-UPPP');
 console.log(env.GAPI_EMAIL);
 console.log(env.GAPI_SCOPE);
 console.log(env.GAPI_KEY);
-// const api = new GoogleAPI({ //Expiration and aud are optional
-//   email: GAPI_EMAIL,
-//   scope: [GAPI_SCOPE],
-//   key: GAPI_KEY,
-// });
-// const x = await api.get(
-// 'https://sheets.googleapis.com/v4/spreadsheets/1Eahbvn759k_wnyv6jq1DVFi61YCIbekq3Rs7EhsV01A?fields=sheets.properties.title'
-// );
-// console.log(x);
+const api = new GoogleAPI({ //Expiration and aud are optional
+  email: GAPI_EMAIL,
+  scope: [GAPI_SCOPE],
+  key: GAPI_KEY,
+});
+const x = await api.get(
+'https://sheets.googleapis.com/v4/spreadsheets/1Eahbvn759k_wnyv6jq1DVFi61YCIbekq3Rs7EhsV01A?fields=sheets.properties.title'
+);
+console.log(x);
 
 router
 .get('/', (ctx) => { //Appends required text to address and redirect
